bbcef2f3096d03696a855e69586cb8ce33f6066d
Manvi07 <b17092@students.iitmandi.ac.in>
2019-04-06 17:52:07 +0530

    Registeration Form changed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, IntegerField
 from passlib.context import CryptContext
 from functools import wraps
 cryptcontext = CryptContext(schemes=["sha256_crypt", "md5_crypt", "des_crypt"])
@@ -36,6 +36,12 @@ class RegisterForm(Form):
     username = StringField('Hospital ID', [validators.Length(min=1, max=50), validators.DataRequired()])
     password = PasswordField('Password', [validators.Length(min=6, max=50), validators.DataRequired(), validators.EqualTo('confirm', message='Passwords Do not match')])
     confirm = PasswordField('Confirm Password', [validators.DataRequired()])
+    rooms = IntegerField('Total Rooms', [validators.DataRequired()])
+    vacant_rooms = IntegerField('Vacant Rooms', [validators.DataRequired()])
+    wards = IntegerField('Total Wards', [validators.DataRequired()], render_kw={"style": "display:inline;"})
+    vacant_wards = IntegerField('Vacant Wards', [validators.DataRequired()])
+    ICUs = IntegerField('Total ICUs', [validators.DataRequired()])
+    vacant_ICUs = IntegerField('Vacant ICUs', [validators.DataRequired()])
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -44,10 +50,16 @@ def register():
         name = form.name.data
         username = form.username.data
         password = cryptcontext.hash(str(form.password.data))
-
+        rooms = form.rooms.data
+        wards = form.wards.data
+        icus = form.icus.data
+        vacant_rooms = form.vacant_rooms.data
+        vacant_wards = form.vacant_wards.data
+        vacant_icus = form.vacant_icus.data
         #Create Cursor
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO Login(hname, hid, password) Values(%s, %s, %s)", (name, username, password))
+        cur.execute("INSERT INTO hospitals(hid, num_of_rooms, num_of_wards, num_of_icus, vacant_rooms, vacant_icus, vacant_wards) Values(%s, %s, %s, %s, %s, %s, %s)", (username, int(rooms), int(wards), int(icus), int(vacant_rooms), int(vacant_icus), int(vacant_wards)))
         mysql.connection.commit()
 
         #Close Connection
